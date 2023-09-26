ac4768e1ac69f7577d695ba2d058d3854be13c9d
Rafael Ayala <rafayala30@gmail.com>
2023-09-26 16:58:12 -0400

    removed CLI prelim application. will be located in nomatic-server repo

--- nomatic-web/public/CLI/client/matchmaker/model/athlete/Belt.java
+++
@@ -1,6 +0,0 @@
-package com.rayala30.nomatic.client.matchmaker.model.athlete;
-
-public enum Belt {
-    // create belt colors
-    White, Blue, Purple, Brown, Black
-}
--- nomatic-web/public/CLI/client/matchmaker/model/athlete/Weight.java
+++
@@ -1,8 +0,0 @@
-package com.rayala30.nomatic.client.matchmaker.model.athlete;
-
-public enum Weight {
-    // create weight classes
-    Roosterweight, Light_Featherweight, Featherweight, Lightweight, Middleweight, Medium_Heavyweight, Heavyweight,
-    Super_Heavyweight, Ultra_Heavyweight, Open_Weight
-
-}
